8389793e2b97fbad1a401edfcb289cd1ab6d4b69
Ariel K <ar.kogan@gmail.com>
2020-09-19 17:45:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,9 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn){
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== findFn) {
+      return null
+    }
+  }
 }
