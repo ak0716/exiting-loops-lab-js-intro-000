46715f9ce7e27d516af5c85777479d4efb1176cd
Ariel K <ar.kogan@gmail.com>
2020-09-19 17:26:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] !== stopValue) {
       array.splice(array[i], changeValue)
-    } else if (array[i] === stopValue) {
-      return array
-    }
+    } return array
   }
 }
 
