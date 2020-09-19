2d1bb6860d4813829d53623598e4fff423021b26
Ariel K <ar.kogan@gmail.com>
2020-09-19 17:02:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      let array[i] = changeValue
+    } else if (array[i] === stopValue) {
+      break;
+    }
+  }
+}
+
+function keepGoing(array, changeValue, skipValue) {
+
+}
+
+findBy(array, findFn){
+
+}
