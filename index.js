93ed4d89b981c4fad8efe9fe22ec3de6b85b86ba
Ariel K <ar.kogan@gmail.com>
2020-09-19 18:35:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn){
   for (let i = 0; i < array.length; i++) {
-    if (array[i] !== findFn) {
-      continue
-    } else if (array[i] === findFn){
+    if (findFn(array[i])) {
       return array[i]
     } return null
   }
